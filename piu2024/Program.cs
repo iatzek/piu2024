f64f6cecb28fcd57d34e5290ee30b1f9ba5f756f
Jacek Kawa <jacek.kawa@polsl.pl>
2025-03-26 12:43:25 +0100

    początek pracy

---
+++ piu2024/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace piu2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
