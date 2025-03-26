bfbc8b40f2e7d0ee2801d2a14d2b6ceccd0046e3
Jacek Kawa <jacek.kawa@polsl.pl>
2025-03-26 13:08:35 +0100

    ujednolicenie pisowni

--- piu2024/Program.cs
+++ piu2024/Program.cs
@@ -18,11 +18,11 @@ namespace piu2024
 
             if (delta < 0)
             {
-                Console.WriteLine("brak rozwiazan");
+                Console.WriteLine("Brak rozwiazan");
             } else
             {
-                Console.WriteLine($"rozwiazania to {(-b - Math.Sqrt(delta)) / (2 * a)}");
-                Console.WriteLine($"rozwiazania to {(-b + Math.Sqrt(delta)) / (2 * a)}");
+                Console.WriteLine($"Rozwiazania to {(-b - Math.Sqrt(delta)) / (2 * a)}");
+                Console.WriteLine($"Rozwiazania to {(-b + Math.Sqrt(delta)) / (2 * a)}");
             }
 
 
