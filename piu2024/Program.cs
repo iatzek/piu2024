644a10a036b273e178c345cde4d8970606b8b5c8
Jacek Kawa <jacek.kawa@polsl.pl>
2025-03-26 12:50:42 +0100

    pierwsza działająca wersja

--- piu2024/Program.cs
+++ piu2024/Program.cs
@@ -6,7 +6,19 @@ namespace piu2024
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ello World!");
+            double a = 1, b = 2, c = -16;
+            double delta = b * b - 4 * a * c;
+
+            if (delta < 0)
+            {
+                Console.WriteLine("brak rozwiazan");
+            } else
+            {
+                Console.WriteLine($"rozwiazania to {(-b - Math.Sqrt(delta)) / 2 * a}");
+                Console.WriteLine($"rozwiazania to {(-b + Math.Sqrt(delta)) / 2 * a}");
+            }
+
+
         }
     }
 }
